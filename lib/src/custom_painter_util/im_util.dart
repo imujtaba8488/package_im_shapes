9743c3a7fcecfc7c6c2704b84a4bb925406c755a
imujtaba8488_mac <developer.imujtaba@gmail.com>
2020-10-03 16:29:09 +0530

    Added pointsOffsetsOnArc and pointOffsetOnArc

--- lib/src/custom_painter_util/im_util.dart
+++ lib/src/custom_painter_util/im_util.dart
@@ -42,3 +42,87 @@ double partOf({
 
   return whichPart / howManyParts * length;
 }
+
+/// Returns the offsets of points when an arc is divided into [numberOfPoints] in XY Cartesian Coordinate System.
+///
+/// [center] Where the center of the arc is located.\
+/// [radius] The radius of the arc.\
+/// [startAngle] Angle __in degrees__ at which the arc should start.\
+/// [sweepAngle] Angle __in degrees__ at which the arc should move.\
+/// [numberOfPoints] The number of points into which to divide the arc.
+///
+/// __Note:__ startAngle and sweepAngle must be greater than or equal to 0.0 and less than or equal to 360.0 degrees.
+///
+/// The idea behind this helper function is to provide the coordinates of points in XY cartesian system, if you were to draw an imaginary arc and divide the arc into 'n' parts. __Note:__ This function in no way draws anything on the screen. All it does is to given you the coordinates of points on an arc. It is upto the user of this function to use these coordinates in whatever ways required.
+List<Offset> pointsOffsetsOnArc({
+  @required Offset center,
+  @required double radius,
+  @required double startAngle,
+  @required double sweepAngle,
+  @required int numberOfPoints,
+}) {
+  assert(
+    startAngle >= 0.0 && startAngle <= 360.0,
+    'The value of startAngle be from 0.0 to 360.0',
+  );
+
+  assert(
+    sweepAngle >= 0.0 && sweepAngle <= 360.0,
+    'The value of sweepAngle must be from 0.0 to 360.0',
+  );
+
+  assert(
+    numberOfPoints >= 0,
+    'The value of points must be greater than or equal to 0',
+  );
+
+  assert(radius > 0.0, 'The value of radius must be greater than 0');
+
+  List<Offset> coordinates = [];
+
+  // This one is manual, since the moveAngle is always going to start past the startAngle.
+  coordinates.add(
+    pointOffsetOnArc(
+      center: center,
+      radius: radius,
+      sweepAngle: toRadians(startAngle),
+    ),
+  );
+
+  // Distance in angle between each coordinate.
+  final double divisionAngle = toRadians(sweepAngle / numberOfPoints);
+
+  // Determines how points should be placed along the arc and how their coordinates should be calculated.
+  double moveAngle = toRadians(startAngle) + divisionAngle;
+
+  for (int i = 0; i < numberOfPoints; i++) {
+    coordinates.add(
+      pointOffsetOnArc(
+        center: center,
+        radius: radius,
+        sweepAngle: moveAngle,
+      ),
+    );
+
+    // To moveAngle add the divisionAngle i.e. the distance in angle between each coordinate.
+    moveAngle += divisionAngle;
+  }
+
+  return coordinates;
+}
+
+/// Returns the offset of a single point on an arc.
+Offset pointOffsetOnArc({
+  Offset center,
+  double radius,
+  double sweepAngle,
+}) {
+  double xCoord = center.dx + (radius - center.dx) * cos(sweepAngle);
+  double yCoord = center.dy + (radius - center.dy) * sin(sweepAngle);
+
+  return Offset(xCoord, yCoord);
+}
+
+double arcLength(double sweepAngle, double radius) {
+  return sweepAngle / toRadians(360) * 2 * pi * radius;
+}
